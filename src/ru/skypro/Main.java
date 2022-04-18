e50509f01a9bdf5a360284fda1aba50eb53bd37e
ArtemCH240322 <sun2383@mail.ru>
2022-04-18 12:11:33 +0300

    Первый комид массив 2. Задание 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,26 @@
 package ru.skypro;
 
+import java.util.Arrays;
+
 public class Main {
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[38];
+        for (int i =0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr= generateRandomArray();
+        System.out.println("Наш массив");
+        System.out.println(Arrays.toString(arr));
+
+        float allSum = 0;
+        for (int i : arr) {
+            allSum +=i;
+        }
+        System.out.println("Сумма трат за месяц составила " + allSum + " руюлей");
     }
 }
